ec96abc0c84d3901a5515fcecec910c7c5f1d8a4
Webfox <patrick@bohner.nl>
2013-09-10 12:27:22 +0200

    Created project.

---
+++ Damspel/Damspel/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Damspel
+//
+//  Created by Patrick Böhner on 9/10/13.
+//  Copyright (c) 2013 KB50-1. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
